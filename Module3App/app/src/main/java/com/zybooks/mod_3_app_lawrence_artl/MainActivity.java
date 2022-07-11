e8fbae5826e5791e65ace8c86ca95d8eb5ff924c
Lorenzo <lorenzo.joseph.dominguez@gmail.com>
2022-07-11 12:43:45 -0700

    created new Android project for Module 3

---
+++ Module3App/app/src/main/java/com/zybooks/mod_3_app_lawrence_artl/MainActivity.java
@@ -0,0 +1,14 @@
+package com.zybooks.mod_3_app_lawrence_artl;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
