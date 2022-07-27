dfabcb6c00632e3abc8b6cec87efea0408c62299
Lorenzo <lorenzo.joseph.dominguez@gmail.com>
2022-07-26 21:31:25 -0700

    Built employee database db schema and UI for adding

---
+++ Book_Database/app/src/main/java/com/zybooks/book_database/MyDatabase.java
@@ -0,0 +1,45 @@
+package com.zybooks.book_database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class MyDatabase extends SQLiteOpenHelper {
+
+    private Context context;
+    private static final String DATABASE_NAME = "Employees.db";
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String TABLE_NAME = "my_library";
+    private static final String COLUMN_ID = "_id";
+    private static final String COLUMN_NAME = "name";
+    private static final String COLUMN_POSITION = "position";
+    private static final String COLUMN_PHONE = "phone_number";
+
+
+
+    public MyDatabase(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        this.context = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        String query =
+                "CREATE TABLE " + TABLE_NAME +
+                        " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        COLUMN_NAME + " TEXT, " +
+                        COLUMN_POSITION + " TEXT, " +
+                        COLUMN_PHONE + " TEXT);";
+        db.execSQL(query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+}
